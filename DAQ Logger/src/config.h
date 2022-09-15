f3e5d6f538cc263176722ae9f4ee0304ae598256
Firstarrow <63761761+Firstarrow@users.noreply.github.com>
2022-09-15 10:15:09 -0600

    Added arbitrary pins for 2 sensors
    Added arbitrary pins for the quadrature speed sensor and the infrared brake temperature sensor.

--- DAQ Logger/src/config.h
+++ DAQ Logger/src/config.h
@@ -1,1 +1,15 @@
 #define RADIO_BAUD 57600; // Baud rate for radio
+
+//Current pin assignments are arbritrary change upon physical assignment
+
+//Quadrature Speed Sensor Pins: Channel B will take precedence if speed is negative
+#define QUAD_VSUPPLY 2;
+#define QUAD_GROUND 3;
+#define QUAD_CHANNELA 4;
+#define QUAD_CHANNELB 5;
+
+//Infrared Brake Temperature Sensor
+#define BRAKETEMP_VSUPPLY 6;
+#define BRAKETEMP_GROUND 7;
+#define BRAKETEMP_SCLOCK 8;
+#define BRAKETEMP_IO 9;

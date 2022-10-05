d563a218202ee92089e5fb25df567d121fde8108
Firstarrow <ceanncliste@gmail.com>
2022-10-04 18:56:06 -0600

    Placed files into the correct directories

--- DAQ Logger/src/main.cpp
+++ DAQ Logger/src/main.cpp
@@ -1,30 +1,9 @@
 #include <Arduino.h>
-#include <stdio.h>
-
-#include "config.h"
-#include "sensors.hpp"
-#include "sdlogging.h"
-#include "irtherm.h"
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-
-  createLog();
+  // put your setup code here, to run once:
 }
 
-//This version is testing connection with quadrature sensor
 void loop() {
-  QuadSensor vsense;
-  double revtime;
-  char logWrite [50];
-  
-  for (int i = 0; i < 10; i++){
-    revtime = vsense.calculateRev();
-    Serial.println("Revolutions per Second: ");
-    Serial.println(revtime);
-
-    sprintf(logWrite, "%d Revolutions per Second: %f", millis(), revtime);
-    writeToLog(logWrite);
-  }
+  // put your main code here, to run repeatedly:
 }
--- DAQ Sensor Front/src/main.cpp
+++ DAQ Sensor Front/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <stdio.h>
+
+#include "config.h"
+#include "sensors.hpp"
+#include "sdlogging.h"
+#include "irtherm.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);
+
+  createLog();
 }
 
+//This version is testing connection with quadrature sensor
 void loop() {
-  // put your main code here, to run repeatedly:
+  QuadSensor vsense;
+  double revtime;
+  char logWrite [50];
+  
+  for (int i = 0; i < 10; i++){
+    revtime = vsense.calculateRev();
+    Serial.println("Revolutions per Second: ");
+    Serial.println(revtime);
+
+    sprintf(logWrite, "%d Revolutions per Second: %f", millis(), revtime);
+    writeToLog(logWrite);
+  }
 }
